b315c5dcec70484934a7faaa50d2dd018175463b
saifali-khan <khansaifali962@gmail.com>
2022-05-31 17:52:25 +0530

    updated some codes in a new way

--- exersice_1/index.js
+++ exersice_1/index.js
@@ -149,9 +149,9 @@
 //   for (i = 0; i < number; i++) {
 //     if (i % 3 === 0 || i % 5 === 0) {
 //       sum += i;
-//       console.log(sum)
 //     }
 //   }
+//   console.log(sum)
 //   return sum
 // }
 // findSum(1000)
