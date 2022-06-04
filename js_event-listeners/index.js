4f4782527a66bd19e1ea5b5c7b7add21bdaa41dc
saifali-khan <khansaifali962@gmail.com>
2022-06-04 18:23:15 +0530

    created a javascript events layout

---
+++ js_event-listeners/index.js
@@ -0,0 +1,8 @@
+let btn = document.getElementById("jsstyle");
+let para = document.getElementById("text");
+
+function js_style() {
+    para.style.color = "green";
+  para.style.fontSize = "20px";
+  para.style.margin = "0 auto";
+}
