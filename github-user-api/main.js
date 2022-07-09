7ea31cdb0635ed944493ae89097b60bf09ab5a5b
saifali-khan <khansaifali962@gmail.com>
2022-07-09 22:08:53 +0530

    created a github user list layout using html, css and javascript fetch api

---
+++ github-user-api/main.js
@@ -0,0 +1,67 @@
+
+let form = document.getElementById("myform");
+form.addEventListener("submit",  function (event) {
+  
+  
+  event.preventDefault()
+  let search = document.getElementById("search")
+  
+  let removespace = search.value.split(" ").join("")
+  
+  
+  fetch("https://api.github.com/users/" + removespace)
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById("profile-card").style.display = "flex";
+    let name = document.getElementById("user-name");
+    let gitProfile = document.getElementById("git-profile");
+    let gitLink = document.getElementById("git-link");
+    let bio = document.getElementById("bio");
+    let repo = document.getElementById("repository");
+    let followers = document.getElementById("followers");
+    let following = document.getElementById("following");
+    let location = document.getElementById("location");
+    let twitter = document.getElementById("twitter");
+    let blog = document.getElementById("blog");
+    let company = document.getElementById("company");
+
+    name.innerHTML = data.name || data.login;
+
+    gitProfile.href = data.html_url;
+    gitProfile.innerHTML = `<img src='${data.avatar_url}' alt='user image' '>`
+
+    gitLink.href = data.html_url;
+    gitLink.innerHTML = "@" + data.login;
+
+    bio.innerHTML ="bio: " + data.bio;
+    if (data.bio === null) {
+      bio.innerHTML = "bio: not available"
+    }
+
+    repo.innerHTML = data.public_repos;
+
+    followers.innerHTML = data.followers;
+
+    following.innerHTML = data.following;
+
+    location.innerHTML = data.location;
+    if (data.location === null) {
+      location.innerHTML = "not available"
+    }
+
+    twitter.innerHTML = data.twitter_username;
+    if (data.twitter_username === null) {
+      twitter.innerHTML += "not avilable"
+    }
+
+    blog.href = data.blog;
+
+    company.innerHTML = data.company;
+    if (data.company === null) {
+      company.innerHTML = "not available"
+    }
+  })
+
+search.value = "";
+})
+
