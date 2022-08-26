cd4c150a484deece3263a9f28047fce81dd57c60
saifali-khan <khansaifali962@gmail.com>
2022-08-26 17:49:58 +0530

    created a github user profile with api key fetching using html, css, and javascript

--- github-user-api/main.js
+++ github-user-api/main.js
@@ -1,67 +1,79 @@
 
 let form = document.getElementById("myform");
-form.addEventListener("submit",  function (event) {
-  
-  
+form.addEventListener("submit", function (event) {
+
+
   event.preventDefault()
   let search = document.getElementById("search")
-  
-  let removespace = search.value.split(" ").join("")
-  
-  
-  fetch("https://api.github.com/users/" + removespace)
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById("profile-card").style.display = "flex";
-    let name = document.getElementById("user-name");
-    let gitProfile = document.getElementById("git-profile");
-    let gitLink = document.getElementById("git-link");
-    let bio = document.getElementById("bio");
-    let repo = document.getElementById("repository");
-    let followers = document.getElementById("followers");
-    let following = document.getElementById("following");
-    let location = document.getElementById("location");
-    let twitter = document.getElementById("twitter");
-    let blog = document.getElementById("blog");
-    let company = document.getElementById("company");
+  let url = "https://api.github.com/users/";
+  let removeSpace = search.value.split(" ").join("")
 
-    name.innerHTML = data.name || data.login;
 
-    gitProfile.href = data.html_url;
-    gitProfile.innerHTML = `<img src='${data.avatar_url}' alt='user image' '>`
+  fetch(url + removeSpace)
+    .then(response => response.json())
+    .then(data => {
+      if (data.message === "Not Found") {
+        
+        alert("No Such User");
+      } else {
 
-    gitLink.href = data.html_url;
-    gitLink.innerHTML = "@" + data.login;
+        document.getElementById("profile-card").style.display = "flex";
+        let name = document.getElementById("user-name");
+        let gitProfile = document.getElementById("git-profile");
+        let gitLink = document.getElementById("git-link");
+        let bio = document.getElementById("bio");
+        let repo = document.getElementById("repository");
+        let followers = document.getElementById("followers");
+        let following = document.getElementById("following");
+        let location = document.getElementById("location");
+        let twitter = document.getElementById("twitter");
+        let blog = document.getElementById("blog");
+        let company = document.getElementById("company");
 
-    bio.innerHTML ="bio: " + data.bio;
-    if (data.bio === null) {
-      bio.innerHTML = "bio: not available"
-    }
+        name.innerHTML = data.name || data.login;
 
-    repo.innerHTML = data.public_repos;
+        gitProfile.href = data.html_url;
+        gitProfile.innerHTML = `<img src='${data.avatar_url}' alt='user image' '>`
 
-    followers.innerHTML = data.followers;
+        gitLink.href = data.html_url;
+        gitLink.innerHTML = "@" + data.login;
 
-    following.innerHTML = data.following;
+        bio.innerHTML = "bio: " + data.bio;
+        if (data.bio === null) {
+          bio.innerHTML = "bio: not available"
+        }
 
-    location.innerHTML = data.location;
-    if (data.location === null) {
-      location.innerHTML = "not available"
-    }
+        repo.innerHTML = data.public_repos;
 
-    twitter.innerHTML = data.twitter_username;
-    if (data.twitter_username === null) {
-      twitter.innerHTML += "not avilable"
-    }
+        followers.innerHTML = data.followers;
 
-    blog.href = data.blog;
+        following.innerHTML = data.following;
 
-    company.innerHTML = data.company;
-    if (data.company === null) {
-      company.innerHTML = "not available"
-    }
-  })
+        location.innerHTML = " " + data.location;
+        if (data.location === null) {
+          location.innerHTML = " not available"
+        }
 
-search.value = "";
+        twitter.innerHTML = ' ' + data.twitter_username;
+        if (data.twitter_username === null) {
+          twitter.innerHTML = " not avilable"
+        }
+
+        blog.href = data.blog;
+        if (data.blog === "" || null) {
+          blog.innerHTML = "not available";
+        }
+
+        company.innerHTML = ' ' + data.company;
+        if (data.company === null) {
+          company.innerHTML = " not available"
+        }
+        
+
+      }
+
+    })
+    .catch(document.getElementById("profile-card").style.display = "none")
+  search.value = "";
 })
 
